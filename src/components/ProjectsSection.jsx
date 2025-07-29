4c9036e60a4434ce1268e789c41e6058d8f9853b
ਪਵਨਜੋਤ ਸਿੰਘ ਦਾ ਮੈਕਬੁੱਕ ਏਅਰ 🙌 <dotimely@Pawanjot-Singhs-macbook-air.local>
2025-07-29 14:06:45 +0800

    Added dummy project pics- implemented projects section with dummy data

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,97 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react"
+
+const projects = [
+    {
+        id: 1,
+        title: "one",
+        description: "desciption of project one",
+        image: "/projects/project1.jpg",
+        tags: ["Typescript", "TailwindCSS", "Supabase"],
+        demoUrl: "#",
+        githubUrl: "#"
+    },
+    {
+        id: 2,
+        title: "two",
+        description: "desciption of project two",
+        image: "/projects/project2.jpg",
+        tags: ["Typescript", "TailwindCSS", "Supabase"],
+        demoUrl: "#",
+        githubUrl: "#"
+    },
+    {
+        id: 3,
+        title: "three",
+        description: "desciption of project three",
+        image: "/projects/project3.jpg",
+        tags: ["Typescript", "TailwindCSS", "Supabase"],
+        demoUrl: "#",
+        githubUrl: "#"
+    }
+]
+
+export const ProjectsSection = () => {
+
+    return <section id="projects" className="py-24 px-4 relative">
+        <div className="container mx-auto max-w-5xl">
+            <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">Featured <span className="text-primary">Projects</span>
+            </h2>
+
+            <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+                iosdgiosogh oshdgiosg oihsdgio osidghioshdgoih sdoighoisdhgoishdg ooio
+            </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {projects.map((project, key) => (
+                    <div
+                        key={key}
+                        className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+                        <div className="h-48 overflow-hidden">
+                            <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" />
+                        </div>
+
+                        <div className="p-4">
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {project.tags.map((tag) => (
+                                    <span className="px-2 py-1 text-xs font-medium rounded-full bg-secondary border text-secondary-foreground">
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
+
+                            <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
+
+                            <p className="text-muted-foreground text-sm mb-4">
+                                {project.description}
+                            </p>
+
+                            <div className="flex justify-between items-center">
+                                <div className="flex space-x-3">
+                                    <a href={project.demoUrl}
+                                        className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                        target="_blank">
+                                        <ExternalLink size={20} />
+                                    </a>
+                                    <a href={project.githubUrl}
+                                        className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                        target="_blank">
+                                        <Github size={20} />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <div className="text-center mt-12">
+                <a className="cosmic-button w-fit flex items-center mx-auto gap-2"
+                    href="https://github.com"
+                    target="_blank">
+                    Check my GitHub <ArrowRight size={16} />
+                </a>
+            </div>
+
+        </div>
+    </section>
+}
