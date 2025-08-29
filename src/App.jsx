9ad18b76d1dedf6f44b7ce54a6d54125167f2dae
ਪਵਨਜੋਤ ਸਿੰਘ ਦਾ ਮੈਕਬੁੱਕ ਏਅਰ 🙌 <dotimely@pawanjoinghsair.home>
2025-08-29 14:00:12 +0800

    Revert "imported vercel in correct file now"
    This reverts commit e411b76a0df1dec6565492bcc4e607bd11c0c693.
    
    Reverted bad code

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { NotFound } from "./pages/NotFound"
 import { Home } from "./pages/Home"
 import { Toaster } from "./components/ui/toaster";
 import { PastExp } from "./pages/PastExp";
+import { Analytics } from "@vercel/analytics/next"
 
 
 
@@ -17,6 +18,7 @@ function App() {
         <Route index element={<Home/>}/>
         <Route path="/PastExp" element={<PastExp/>}/>
         <Route path="*" element={<NotFound/>}/>
+        <Analytics />
       </Routes>
       </BrowserRouter>
     </>
