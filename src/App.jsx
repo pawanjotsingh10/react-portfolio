e411b76a0df1dec6565492bcc4e607bd11c0c693
ਪਵਨਜੋਤ ਸਿੰਘ ਦਾ ਮੈਕਬੁੱਕ ਏਅਰ 🙌 <dotimely@pawanjoinghsair.home>
2025-08-29 13:48:45 +0800

    imported vercel in correct file now

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { NotFound } from "./pages/NotFound"
 import { Home } from "./pages/Home"
 import { Toaster } from "./components/ui/toaster";
 import { PastExp } from "./pages/PastExp";
-import { Analytics } from "@vercel/analytics/next"
 
 
 
@@ -18,7 +17,6 @@ function App() {
         <Route index element={<Home/>}/>
         <Route path="/PastExp" element={<PastExp/>}/>
         <Route path="*" element={<NotFound/>}/>
-        <Analytics />
       </Routes>
       </BrowserRouter>
     </>
